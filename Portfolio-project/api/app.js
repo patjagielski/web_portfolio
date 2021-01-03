3ba3242da66c91fdefc9cb8479dee61c380c0b4f
patjagielski <jagielskipat@gmail.com>
2021-01-03 02:10:49 +0100

    changed from mongoDB to mysqlDB, connected backend to frontend

--- Portfolio-project/api/app.js
+++ Portfolio-project/api/app.js
@@ -1,17 +1,53 @@
-const { MongoClient } = require("mongodb");
- 
-// Replace the following with your Atlas connection string                                                                                                                                        
-const url = "mongodb+srv://admin1:admin123@portfoliodb.eeggc.mongodb.net/PortfolioDB?retryWrites=true&w=majority";
-const client = new MongoClient(url);
-async function run() {
-    try {
-        await client.connect();
-        console.log("Connected correctly to server");
-    } catch (err) {
-        console.log(err.stack);
+const mysql = require('mysql');
+const express = require('express');
+
+const app = express();
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password:"pass",
+  database:"tin_db",
+  port:"3306"
+})
+
+connection.connect((err) =>{
+  if(err){
+    throw err
+  }else{
+    console.log("connected");
+  }
+})
+
+app.get("/checkLogin",(req, res)=>{
+  var username = req.username;
+  var password = req.password;
+  connection.query("SELECT EXISTS(SELECT * FROM user WHERE username=? and password=?", [username, password], (err, rows)=>{
+    if(err){
+      throw err
+    }else{
+      return true;
     }
-    finally {
-        await client.close();
-    }
-}
-run().catch(console.dir);
+  })
+})
+
+// connection.query('CREATE TABLE tabletest(id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY, something VARCHAR(255) NOT NULL)', (err,rows)=>{
+//   if(err){
+//     throw err
+//   }else{
+//     console.log("YEEEEEE")
+//     console.log(rows);
+//   }
+// })
+connection.query("INSERT INTO tabletest(id, something) VALUES (2, 'yeehawwwww')", (err, rows)=>{
+  if(err){
+    throw err
+  }else{
+    console.log("YYEEE")
+    console.log(rows);
+  }
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log("app is listening to " + port)
---
+++ Portfolio-project/client/src/actions/auth.js
@@ -0,0 +1,5 @@
+export const startLogin = () =>{
+    return () =>{
+        //begin login 
+    };
+}
